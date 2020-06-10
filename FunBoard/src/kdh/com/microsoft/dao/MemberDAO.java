82c0ac354431cecad0f3eb02bf39b43305e2fddc
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:44:06 +0900

    dao list메소드추가

--- FunBoard/src/kdh/com/microsoft/dao/MemberDAO.java
+++ FunBoard/src/kdh/com/microsoft/dao/MemberDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -42,7 +44,29 @@ public class MemberDAO {
 			closeAll(null, pstmt, conn);
 		}
 	}
-
+	public List<MemberDTO> selectAll() {
+		List<MemberDTO> list = new ArrayList<MemberDTO>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from member order by id";
+		try {
+			conn = dataFactory.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				list.add(new MemberDTO(id, name, age, null));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeAll(rs, pstmt, conn);
+		}
+		return list;
+	}
 	private void closeAll(ResultSet rs, PreparedStatement pstmt, Connection conn) {
 		try {
 			if (rs != null)
