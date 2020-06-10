ad9f1be34916906f8f402fe54b144cd3f50bcb88
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:04:41 +0900

    LoginUICommand생성

---
+++ FunBoard/src/kdh/com/microsoft/command/LoginUICommand.java
@@ -0,0 +1,17 @@
+package kdh.com.microsoft.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginUICommand implements Command {
+
+	@Override
+	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return new CommandAction(true, "./kdhjsp/loginui.jsp");
+	}
+
+}
