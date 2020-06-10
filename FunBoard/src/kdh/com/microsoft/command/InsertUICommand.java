807bca64f6a9623274649d49f22d595c24db1116
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:11:27 +0900

    insertui커맨드

---
+++ FunBoard/src/kdh/com/microsoft/command/InsertUICommand.java
@@ -0,0 +1,17 @@
+package kdh.com.microsoft.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InsertUICommand implements Command {
+
+	@Override
+	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return new CommandAction(true, "./kdhjsp/insert.jsp");
+	}
+
+}
