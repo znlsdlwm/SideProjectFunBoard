c376b7727dcf37118983ee190cf8ea966c5a528b
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:00:28 +0900

    calcUI 추가

---
+++ FunBoard/src/kdh/com/microsoft/command/CalculatorUICommand.java
@@ -0,0 +1,18 @@
+package kdh.com.microsoft.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalculatorUICommand implements Command {
+
+	@Override
+	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return new CommandAction(false, "./kdhjsp/kdhcalc.jsp");
+	}
+
+}
