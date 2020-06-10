179209d39ca98ff7d81d82d9161f6b60c14ea00d
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:30:12 +0900

    insertCommand dao반영

--- FunBoard/src/kdh/com/microsoft/command/InsertCommand.java
+++ FunBoard/src/kdh/com/microsoft/command/InsertCommand.java
@@ -6,11 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kdh.com.microsoft.dao.MemberDAO;
+import member.domain.MemberDTO;
+
 public class InsertCommand implements Command {
 
 	@Override
 	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String sAge = request.getParameter("age");
+		String pw = request.getParameter("pw");
+		int age = 0;
+		if(sAge!=null) {
+			age = Integer.parseInt(sAge);
+		}
+		MemberDAO dao = new MemberDAO();
+		dao.insert(new MemberDTO(id, name, age, pw));
 		return new CommandAction(true, "./home.kdh");
 	}
 
