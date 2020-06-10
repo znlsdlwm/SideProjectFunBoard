fdfb2569101fff97b7340960b301879bbbba2fca
cloudydong <cloudy12028@gmail.com>
2020-06-10 09:25:13 +0900

    CommandAction Class 제작

---
+++ FunBoard/src/kdh/com/microsoft/CommandAction.java
@@ -0,0 +1,25 @@
+package kdh.com.microsoft;
+
+public class CommandAction {
+	private boolean isRedirect;
+	private String where;
+	public CommandAction() {
+	}
+	public CommandAction(boolean isRedirect, String where) {
+		super();
+		this.isRedirect = isRedirect;
+		this.where = where;
+	}
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+	public String getWhere() {
+		return where;
+	}
+	public void setWhere(String where) {
+		this.where = where;
+	}
+}
