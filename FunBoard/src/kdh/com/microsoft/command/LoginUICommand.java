c86aba6df9a5524b9bab892a2ba7a614ac2279ff
cloudydong <cloudy12028@gmail.com>
2020-06-10 17:50:44 +0900

    로그인ui

--- FunBoard/src/kdh/com/microsoft/command/LoginUICommand.java
+++ FunBoard/src/kdh/com/microsoft/command/LoginUICommand.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 public class LoginUICommand implements Command {
 
