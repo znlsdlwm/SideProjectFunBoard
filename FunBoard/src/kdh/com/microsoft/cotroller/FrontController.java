4e5286cb980ee47a5865aee2c7ed1242f5ce30a1
cloudydong <cloudy12028@gmail.com>
2020-06-10 09:30:50 +0900

    FrontController와 kdhhome

---
+++ FunBoard/src/kdh/com/microsoft/cotroller/FrontController.java
@@ -0,0 +1,61 @@
+package kdh.com.microsoft.cotroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kdh.com.microsoft.command.Command;
+import kdh.com.microsoft.command.CommandAction;
+
+/**
+ * Servlet implementation class KDHFrontController
+ */
+@WebServlet("*.kdh")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String ctxp = request.getContextPath();
+		String sp = uri.substring(ctxp.length());
+		
+		Command com = null;
+		
+		if(sp.equalsIgnoreCase("/home.kdh")) {
+			com = new KDHHomeCommand();
+		} else {
+			System.out.println("제공하지 않는 서비스 입니다.");
+		}
+		
+		if (com!=null) {
+			CommandAction action = com.execute(request, response);
+			if (action.isRedirect()) {
+				response.sendRedirect(action.getWhere());
+			} else {
+				request.getRequestDispatcher(action.getWhere()).forward(request, response);
+			} 
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ FunBoard/src/kdh/com/microsoft/cotroller/KDHHomeCommand.java
@@ -0,0 +1,20 @@
+package kdh.com.microsoft.cotroller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kdh.com.microsoft.command.Command;
+import kdh.com.microsoft.command.CommandAction;
+
+public class KDHHomeCommand implements Command {
+
+	@Override
+	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return new CommandAction(true, "kdhhome.jsp");
+	}
+
+}
