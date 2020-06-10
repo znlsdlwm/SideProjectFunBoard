fd6d14ba205a7ffe93647d126097fdbb191d093e
cloudydong <cloudy12028@gmail.com>
2020-06-10 09:27:01 +0900

    Command 인터페이스

---
+++ FunBoard/src/kdh/com/microsoft/Command.java
@@ -0,0 +1,12 @@
+package kdh.com.microsoft;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public interface Command {
+	CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException;
+}
