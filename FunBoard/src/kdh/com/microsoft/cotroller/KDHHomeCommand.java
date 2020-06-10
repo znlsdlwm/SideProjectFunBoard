39ce42e74302701c7d4435d1a57c90d536361d53
cloudydong <cloudy12028@gmail.com>
2020-06-10 09:34:45 +0900

    ./kdh_JSP/kdhhome.jsp jsp폴더 만들고 경로변경

--- FunBoard/src/kdh/com/microsoft/cotroller/KDHHomeCommand.java
+++ FunBoard/src/kdh/com/microsoft/cotroller/KDHHomeCommand.java
@@ -14,7 +14,7 @@ public class KDHHomeCommand implements Command {
 	@Override
 	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		return new CommandAction(true, "kdhhome.jsp");
+		return new CommandAction(false, "./kdh_JSP/kdhhome.jsp");
 	}
 
 }
