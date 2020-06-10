ed52886fab498080a65dd50c9403e2e13f31e506
cloudydong <cloudy12028@gmail.com>
2020-06-10 09:38:16 +0900

    CalculatorCommand ./kdhcalc.jsp 커멘드액션 경로테스트

---
+++ FunBoard/src/kdh/com/microsoft/command/CalculatorCommand.java
@@ -0,0 +1,18 @@
+package kdh.com.microsoft.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalculatorCommand implements Command {
+
+	@Override
+	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		return new CommandAction(false, "./kdhcalc.jsp");
+	}
+
+}
