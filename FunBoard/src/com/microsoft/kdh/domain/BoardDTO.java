922b29de877c0fd5c4acda90da36d0f5736c5730
cloudydong <cloudy12028@gmail.com>
2020-06-11 10:46:44 +0900

    게시판DTO

---
+++ FunBoard/src/com/microsoft/kdh/domain/BoardDTO.java
@@ -0,0 +1,90 @@
+package com.microsoft.kdh.domain;
+
+import java.io.Serializable;
+
+public class BoardDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private int num;
+	private String writer;
+	private String title;
+	private String content;
+	private String writeDay;
+	private int readcnt;
+	private int repRoot;
+	private int repStep;
+	private int repIndent;
+	public BoardDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	public BoardDTO(int num, String writer, String title, String content, String writeDay, int readcnt, int repRoot,
+			int repStep, int repIndent) {
+		super();
+		this.num = num;
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+		this.writeDay = writeDay;
+		this.readcnt = readcnt;
+		this.repRoot = repRoot;
+		this.repStep = repStep;
+		this.repIndent = repIndent;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriteDay() {
+		return writeDay;
+	}
+	public void setWriteDay(String writeDay) {
+		this.writeDay = writeDay;
+	}
+	public int getReadcnt() {
+		return readcnt;
+	}
+	public void setReadcnt(int readcnt) {
+		this.readcnt = readcnt;
+	}
+	public int getRepRoot() {
+		return repRoot;
+	}
+	public void setRepRoot(int repRoot) {
+		this.repRoot = repRoot;
+	}
+	public int getRepStep() {
+		return repStep;
+	}
+	public void setRepStep(int repStep) {
+		this.repStep = repStep;
+	}
+	public int getRepIndent() {
+		return repIndent;
+	}
+	public void setRepIndent(int repIndent) {
+		this.repIndent = repIndent;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
