5080214f8b661084f4d50bdea76cefb85a126b28
cloudydong <cloudy12028@gmail.com>
2020-06-11 17:43:39 +0900

    로그인 패키지정리

--- FunBoard/src/com/microsoft/kdh/log/command/LoginUICommand.java
+++ FunBoard/src/com/microsoft/kdh/log/command/LoginUICommand.java
@@ -1,4 +1,4 @@
-package com.microsoft.kdh.command;
+package com.microsoft.kdh.log.command;
 
 import java.io.IOException;
 
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
+
+import com.microsoft.kdh.command.Command;
+import com.microsoft.kdh.command.CommandAction;
 
 public class LoginUICommand implements Command {
 
--- FunBoard/src/com/microsoft/kdh/log/command/LogoutCommand.java
+++ FunBoard/src/com/microsoft/kdh/log/command/LogoutCommand.java
@@ -1,4 +1,4 @@
-package com.microsoft.kdh.command;
+package com.microsoft.kdh.log.command;
 
 import java.io.IOException;
 
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.microsoft.kdh.command.Command;
+import com.microsoft.kdh.command.CommandAction;
 
 import member.domain.LoginDTO;
 
