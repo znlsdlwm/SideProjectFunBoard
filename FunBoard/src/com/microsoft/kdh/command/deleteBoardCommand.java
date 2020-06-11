cba0ff2561be22a2784fe98bceead1d25fd9bfa1
cloudydong <cloudy12028@gmail.com>
2020-06-11 15:29:05 +0900

    삭제기능추가

--- FunBoard/src/com/microsoft/kdh/command/deleteBoardCommand.java
+++ FunBoard/src/com/microsoft/kdh/command/deleteBoardCommand.java
@@ -6,13 +6,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.microsoft.kdh.dao.BoardDAO;
+
 public class deleteBoardCommand implements Command {
 
 	@Override
 	public CommandAction execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		BoardDAO dao = new BoardDAO();
+		String sNum = request.getParameter("num");
+		int num = -1;
+		if(sNum!=null) {
+			num = Integer.parseInt(sNum);
+		}
+		dao.delete(num);
+		return new CommandAction(true, "listboard.kdh");
 	}
 
 }
