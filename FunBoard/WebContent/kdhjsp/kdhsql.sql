441b92e1a1cc1f0f2a161e5d2c439aae98bef08a
cloudydong <cloudy12028@gmail.com>
2020-06-15 10:20:55 +0900

    sql test

--- FunBoard/WebContent/kdhjsp/kdhsql.sql
+++ FunBoard/WebContent/kdhjsp/kdhsql.sql
@@ -1,4 +1,12 @@
 select * from member
 select * from board
+select * from v_listBoard
+
 
 select * from board where title like '%수정%' order by repRoot desc, repStep
+
+select * from (select rownum rnum, num, title, writer, writeday, readcnt, repIndent from (
+				select * from board where writer like '%%' order by repRoot desc , repStep)) 
+				where rnum>=0 and rnum<=10
+				
+select count(num) from board where title like '%%'
