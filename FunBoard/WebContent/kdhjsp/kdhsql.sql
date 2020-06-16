c8539912dd8dd022b5213b23b1ce1a7a4e355374
cloudydong <cloudy12028@gmail.com>
2020-06-16 16:38:50 +0900

    sql 댓글테이블 추가

--- FunBoard/WebContent/kdhjsp/kdhsql.sql
+++ FunBoard/WebContent/kdhjsp/kdhsql.sql
@@ -1,20 +1,23 @@
 select * from member
 select * from board
-
+select * from b_comment
 create table b_comment(
-c_num number(5) not null primary key,
+c_num number(4) not null primary key,
 c_writer varchar2(21) not null,
 c_content varchar2(1000) not null,
 c_password varchar2(8),
 c_writeday DATE default sysdate,
 c_good number(4) default 0,
 c_bad number(4) default 0,
-c_waning number(4) default 0,
+c_warning number(4) default 0,
 c_Root number(4),
 c_Step number(4),
-c_Indent number(4)
+c_Indent number(4),
+b_num not null,
+constraint fk_bnum foreign key(b_num) references board(num)
 )
 
+alter table b_comment add constraint fk_b_num foreign key(num) references board
 ALTER TABLE board add(
 good number(4) default 0,
 bad number(4) default 0
