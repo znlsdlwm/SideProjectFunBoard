1451d3b4e347727f83133983b7d28b191325f7cc
cloudydong <cloudy12028@gmail.com>
2020-06-17 11:58:45 +0900

    b_event sql 테이블 생성

--- FunBoard/WebContent/kdhjsp/kdhsql.sql
+++ FunBoard/WebContent/kdhjsp/kdhsql.sql
@@ -1,6 +1,37 @@
 select * from member
 select * from board
 select * from b_comment
+select * from b_event
+
+alter table b_comment add constraint fk_b_num foreign key(num) references board
+ALTER TABLE board add(
+good number(4) default 0,
+bad number(4) default 0
+)
+
+
+
+select * from board order by repRoot desc, repStep
+select * from (select rownum rnum, num, title, writer, writeday, readcnt, repIndent from (
+				select * from board where writer like '%%' order by repRoot desc , repStep)) 
+				where rnum>=0 and rnum<=10
+select count(num) from board where title like '%%'
+select * from (select rownum rnum writer from (
+				select * from board order by repRoot desc , repStep)) where writer='m003' and rnum=1
+select * from (select num, writer from (select * from board order by repRoot desc , repStep)) where writer='m003'
+
+
+
+create table b_event(
+b_num number(4) not null,
+m_id varchar2(4) not null,
+b_good number(4) default 0 check (b_good IN(0, 1)),
+b_bad number(4) default 0 check (b_bad IN(0, 1)),
+b_warning number(4) default 0 check (b_warning IN(0, 1)),
+constraint fk_b_bnum foreign key(b_num) references board(num),
+constraint fk_b_mid foreign key(m_id) references member(id)
+)
+
 create table b_comment(
 c_num number(4) not null primary key,
 c_writer varchar2(21) not null,
@@ -16,24 +47,22 @@ c_Indent number(4),
 b_num not null,
 constraint fk_bnum foreign key(b_num) references board(num)
 )
-delete from B_COMMENT
-alter table b_comment add constraint fk_b_num foreign key(num) references board
-ALTER TABLE board add(
-good number(4) default 0,
-bad number(4) default 0
+
+create tabel board(
+num number(4) not null,
+writer varchar2(21) not null,
+title varchar2(60) not null,
+content varchar2(1000),
+writeday DATE default sysdate,
+readcnt number(4),
+reproot number(4),
+repstep number(4),
+repindent number(4),
 )
 
-
-select * from board order by repRoot desc, repStep
-
-select * from (select rownum rnum, num, title, writer, writeday, readcnt, repIndent from (
-				select * from board where writer like '%%' order by repRoot desc , repStep)) 
-				where rnum>=0 and rnum<=10
-				
-select count(num) from board where title like '%%'
-
-
-select * from (select rownum rnum writer from (
-				select * from board order by repRoot desc , repStep)) where writer='m003' and rnum=1
-				
-select * from (select num, writer from (select * from board order by repRoot desc , repStep)) where writer='m003'
+create table member(
+id varchar2(4) not null,
+name varchar2(12) not null,
+age number(3),
+pw varchar2(8) not null
+)
